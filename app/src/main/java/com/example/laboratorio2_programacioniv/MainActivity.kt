c765edc6fc6ebad561ec3a331f8930bddc83ec3e
PaolaMerino <113115113+PaolaMerino@users.noreply.github.com>
2022-10-07 00:15:00 -0600

    Revert "commit"
    This reverts commit 9a5b59ac029b953466ca47adc7ae9f55af981c62.

--- app/src/main/java/com/example/laboratorio2_programacioniv/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio2_programacioniv/MainActivity.kt
@@ -7,11 +7,11 @@ import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var ett_nombre: EditText
-    private lateinit var etn_edad: EditText
-    private lateinit var ett_depto: EditText
-    private lateinit var lv_Datos: ListView
-    private lateinit var tv_Seleccion: TextView
+    private lateinit var etNombre: EditText
+    private lateinit var etnEdad: EditText
+    private lateinit var etDepartamento: EditText
+    private lateinit var lvNombres: ListView
+    private lateinit var tvDatos: TextView
 
     private var nombres = arrayListOf<String>()
     private var edades = arrayListOf<String>()
@@ -19,37 +19,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ett_nombre = findViewById(R.id.ett_nombre)
-        etn_edad = findViewById(R.id.etn_edad)
-        ett_depto = findViewById(R.id.ett_depto)
-        lv_Datos = findViewById(R.id.lv_Datos)
-        tv_Seleccion = findViewById(R.id.tv_Seleccion)
+        etNombre = findViewById(R.id.ett_nombre)
+        etnEdad = findViewById(R.id.etn_edad)
+        etDepartamento = findViewById(R.id.ett_depto)
+        lvNombres = findViewById(R.id.lv_Datos)
+        tvDatos = findViewById(R.id.tv_Seleccion)
 
     }
-    fun Guardar(v: View){
+    fun registrar(v: View){
 
-        val name = ett_nombre.text.toString()
-        val age = etn_edad.text.toString()
-        val department = ett_depto.text.toString()
+        val nombre = etNombre.text.toString()
+        val edad = etnEdad.text.toString()
+        val departamento = etDepartamento.text.toString()
 
-        if(!name.isEmpty() && !age.isEmpty() && !department.isEmpty()){
-            nombres.add(name)
-            edades.add(age)
-            departamentos.add(department)
+        if(!nombre.isEmpty() && !edad.isEmpty() && !departamento.isEmpty()){
+            nombres.add(nombre)
+            edades.add(edad)
+            departamentos.add(departamento)
 
             val adaptador: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.list_progra, nombres)
-            lv_Datos.adapter = adaptador
+            lvNombres.adapter = adaptador
 
-            lv_Datos.onItemClickListener = object : AdapterView.OnItemClickListener{
+            lvNombres.onItemClickListener = object : AdapterView.OnItemClickListener{
                 override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    tv_Seleccion.text = " Nombre: ${lv_Datos?.getItemAtPosition(position)}    Edad: ${edades[position]}años    Departamento: ${departamentos[position]}"
+                    tvDatos.text = " Nombre: ${lvNombres?.getItemAtPosition(position)}    Edad: ${edades[position]}años    Departamento: ${departamentos[position]}"
                 }
             }
 
 
-            ett_nombre.setText("")
-            etn_edad.setText("")
-            ett_depto.setText("")
+            etNombre.setText("")
+            etnEdad.setText("")
+            etDepartamento.setText("")
 
             Toast.makeText(this, "Registro exitoso", Toast.LENGTH_LONG).show()
         }else{
